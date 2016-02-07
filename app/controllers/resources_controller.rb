7fbc1a1ef1a01eeba45261517aaa7b52b7d97466
tvcou <tvcou.zyx77@gmail.com>
2016-02-08 00:09:01 +0900

    "test json data hostinfo page vi resources_controller.rb hostinfo.html.erb"

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,6 +4,15 @@ class ResourcesController < ApplicationController
   end
 
   def hostinfo
+    hostinfodata = '[
+      {"host":"Host-1", "guest": [ {"name":"WEB-1", "mem":"100"}, {"name":"WEB-2", "mem":"200"} ]},
+    {"host":"Host-2", "guest": [ {"name":"WEB-3", "mem":"100"} ]}
+    ]';
+    @hostinfodata_json = JSON.parse(hostinfodata);
+
+    logger.fatal "------------------"
+    logger.debug @hostinfodata_json.inspect
+    logger.fatal "------------------"
   end
 
 end
